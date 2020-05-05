4d9604d5a480c057e44cd4f31e51ecf555bee222
vijaykglg <vijay.gupta7@yahoo.in>
2020-05-05 16:37:56 +0530

    Externalized Configuration in Spring

---
+++ src/main/java/com/vijay/learn/springcore/client/PropertyPlaceHolderClient.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020. All rights reserved .
+ * This code developed by Vijay Gupta for learning purpose only.
+ * Certification: Spring Framework Certification Training
+ */
+
+package com.vijay.learn.springcore.client;
+
+import com.vijay.learn.springcore.externalization.JdbcDataSource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PropertyPlaceHolderClient {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-application-context-autodiscovery.xml");
+        JdbcDataSource datasource = context.getBean("datasource", JdbcDataSource.class);
+        System.out.printf("Database driver name : %s %n", datasource.getDrivername());
+        System.out.printf("Database user name : %s %n", datasource.getUsername());
+        System.out.printf("Database password: %s %n", datasource.getPassword());
+    }
+
+}
